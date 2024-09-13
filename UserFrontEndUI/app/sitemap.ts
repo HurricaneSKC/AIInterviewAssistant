56e2d76261c7f2f206c46fa7c02dc6d95cd1ec9a
Sanjay Chopra <sanjaykchopra@gmail.com>
2024-09-13 16:38:38 +0100

    updates sitemap

--- UserFrontEndUI/app/sitemap.ts
+++ UserFrontEndUI/app/sitemap.ts
@@ -3,11 +3,27 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "",
+      url: "/",
       lastModified: new Date(),
     },
     {
-      url: "/demo",
+      url: "/user/signin",
+      lastModified: new Date(),
+    },
+    {
+      url: "/user/signup",
+      lastModified: new Date(),
+    },
+    {
+      url: "/user/dashboard",
+      lastModified: new Date(),
+    },
+    {
+      url: "/user/dashboard/my-interviews",
+      lastModified: new Date(),
+    },
+    {
+      url: "/interview",
       lastModified: new Date(),
     },
   ];
